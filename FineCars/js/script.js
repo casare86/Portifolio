05bf9071038fb8897a26bb4aa7648b3642ff2b61
Fernando Casare <fercasare@gmail.com>
2020-12-05 19:53:30 -0300

    Adicionado efeito tooltip no mapa de Contato

--- FineCars/js/script.js
+++ FineCars/js/script.js
@@ -2,12 +2,14 @@ import * as menu from "./modules/menu.js";
 import initAnimateScroll from "./modules/scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 menu.initSmoothScroll();
 menu.initTabNav();
 initAccordion();
 initAnimateScroll();
 initModal();
+initTooltip();
 
 
 
