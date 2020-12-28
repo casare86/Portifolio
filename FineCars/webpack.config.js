a6e6f78e3bc0982add120af5150d3d7fa05354a8
Fernando Casare <fercasare@gmail.com>
2020-12-27 22:31:20 -0300

    add babel to the project

--- FineCars/webpack.config.js
+++ FineCars/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/script.js',
+  entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
